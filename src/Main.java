ed2725340ac045f0d997f7fbcda43eaa72f9a3ec
xujiaxiao <xujiaxiao@tellhow.com>
2021-08-24 22:16:30 +0800

    在新分支dev上的第一次提交

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Second commit");
         System.out.println("Three commit");
+        System.out.println("This is my first branch");
     }
 }
